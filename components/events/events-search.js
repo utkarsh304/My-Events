5b3041210906dc33b470acb9d952bf3a60321a18
Utkarsh <utkarsharma304@gmail.com>
2023-09-25 23:27:26 +0530

    Designed a web-application featuring various events using Next.js framework, used api-routes for the backend and for the database, used MongoDb and firebase.

---
+++ components/events/events-search.js
@@ -0,0 +1,58 @@
+import { useRef } from 'react';
+
+import Button from '../ui/button';
+import classes from './events-search.module.css';
+
+function EventsSearch(props) {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);    
+  }
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.controls}>
+        <div className={classes.control}>
+          <label htmlFor='year'>Year</label>
+          <select id='year' ref={yearInputRef}>
+            <option value='2017'>2017</option>
+            <option value='2018'>2018</option>
+            <option value='2019'>2019</option>
+            <option value='2020'>2020</option>
+            <option value='2021'>2021</option>
+            <option value='2022'>2022</option>
+            <option value='2023'>2023</option>
+            <option value='2024'>2024</option>
+          </select>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='month'>Month</label>
+          <select id='month' ref={monthInputRef}>
+            <option value='1'>January</option>
+            <option value='2'>February</option>
+            <option value='3'>March</option>
+            <option value='4'>April</option>
+            <option value='5'>May</option>
+            <option value='6'>June</option>
+            <option value='7'>July</option>
+            <option value='8'>August</option>
+            <option value='9'>Septemer</option>
+            <option value='10'>October</option>
+            <option value='11'>November</option>
+            <option value='12'>December</option>
+          </select>
+        </div>
+      </div>
+      <Button>Find Events</Button>
+    </form>
+  );
+}
+
+export default EventsSearch;
